aa7ad3868ba9c55b0ad0acff7d94662ca83740cd
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-25 19:24:55 +0530

    added

--- js/v-2-buddy.js
+++ js/v-2-buddy.js
@@ -1,1 +1,10 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddies(data))
+}
+loadBuddies()
+const displayBuddies = buddies => {
+   console.log(buddies);
+}
 console.log("hello ! how are you all ?")
