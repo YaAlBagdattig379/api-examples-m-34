2d402fa42dfed79aa3de928d1b7d21897c4936bc
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-27 13:12:13 +0530

    added

--- js/v-4-B-dynamically.js
+++ js/v-4-B-dynamically.js
@@ -3,7 +3,6 @@ const loadCountries = () =>{
     fetch('https://restcountries.com/v2/all')
     .then(res => res.json())
     .then(data =>displayCountries(data));
-
 }
 loadCountries()
 const displayCountries = countries =>{
