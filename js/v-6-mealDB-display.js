e9fd37145b45ed10af78a41bc36a411dd79221c7
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-28 16:40:26 +0530

    added

--- js/v-6-mealDB-display.js
+++ js/v-6-mealDB-display.js
@@ -13,7 +13,7 @@ const searchFood =() =>{
 const displayResults = meals =>{
     const searchResults = document.getElementById("search-results");
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -22,6 +22,7 @@ const displayResults = meals =>{
              <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strInstructions.slice(0,250)}.</p>
+                <button style="color:white;border-radius:7px;border:2px solid;background-color:green;"> buy now </button>
              </div>
            </div
         `
