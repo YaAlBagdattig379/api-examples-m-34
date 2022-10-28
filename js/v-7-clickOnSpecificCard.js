1c5f4c706e53a8df622c27933476f5db6ae9a58d
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-10-28 19:16:47 +0530

    added

--- js/v-7-clickOnSpecificCard.js
+++ js/v-7-clickOnSpecificCard.js
@@ -13,11 +13,14 @@ const searchFood =() =>{
 const displayResults = meals =>{
     const searchResults = document.getElementById("search-results");
     meals.forEach(meal => {
-        // console.log(meal)
+        // console.log(meal.idMeal)
+        // console.log(meal.strMealThumb)
+        // console.log(meal.strYoutube)
+        // console.log(meal.strYoutube)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-          <div class="card h-100">
+          <div onclick="showCardDetails('${meal}')" class="card h-100">
              <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
              <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -25,9 +28,13 @@ const displayResults = meals =>{
                 <button style="color:white;border-radius:7px;border:2px solid;background-color:green;"> buy now </button>
              </div>
            </div
-        `
+        `;
         searchResults.appendChild(div);
         // console.log(meal)
     });
-    // console.log(meals)
+}
+
+// show card details 
+const showCardDetails = cardDetail =>{
+    console.log(cardDetail.idMeal)
 }
